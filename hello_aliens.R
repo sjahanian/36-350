376d10a3e49ba5d7ec536297628411d637467231
Sara Jahanian <Sara@Saras-MBP-4.fios-router.home>
2017-12-10 21:40:03 -0500

    formatting attempt #4792

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,9 +1,9 @@
 cat("Hello, aliens! If you understand this, you 
     likely can contribute more to our civilization than 
     we can to yours. However, here are some digits of what we call 'pi':",
-    3.14159265358979323846264338327950288419716939937510582097494459230781
+    "3.14159265358979323846264338327950288419716939937510582097494459230781
     6406286208998628034825342117067982148086513282306647093844609550582231
-    7253594081284811174502841027019385211055596446, 
+    7253594081284811174502841027019385211055596446", 
     "We have plenty of questions for you from here on Earth: 
     What source of energy do you use?
     What is your atmosphere made of?
