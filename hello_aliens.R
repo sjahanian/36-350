9ef607b78a139b412b9b8d7a869fb2703a78cd49
Sara Jahanian <Sara@Saras-MBP-4.fios-router.home>
2017-12-10 21:09:41 -0500

    Alien message

---
+++ hello_aliens.R
@@ -0,0 +1,22 @@
+Hello, aliens! If you understand this, you 
+likely can contribute more to our civilization than 
+we can to yours. However, here are some digits of what we call 'pi': 
+  3.1415926535897932384626433832795028841971693993751058209749445
+  9230781640628620899862803482534211706798214808651328230664709
+  38446095505822317253594081284811174502841027019385211055596446
+
+We have plenty of questions for you from here on Earth: 
+
+What source of energy do you use?
+What is your atmosphere made of?
+Can you send us a ship?
+Which came first the chicken or the egg?
+
+Here is a link to arguably the best music video ever made: 
+  
+  https://www.youtube.com/watch?v=djV11Xbc914
+
+Please send us something back so we know you received this message!
+  
+Sincerely,
+Your friends from Earth
