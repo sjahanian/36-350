dc2f681beba847fa10f91e42a8ae5abce86853c4
Sara Jahanian <Sara@Saras-MBP-4.fios-router.home>
2017-12-10 21:45:14 -0500

    ugh

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,9 +1,9 @@
-cat("Hello, aliens! If you understand this, you 
-    likely can contribute more to our civilization than 
-    we can to yours. However, here are some digits of what we call 'pi':",
-    "3.14159265358979323846264338327950288419716939937510582097494459230781
-    64062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446", 
-    "We have plenty of questions for you from here on Earth: 
+cat("Hello, aliens! If you understand this, you likely can contribute more to our civilization than we can to yours. However, here are some digits of what we call 'pi':",
+    
+    "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446", 
+    
+    "We have plenty of questions for you from here on Earth:
+
     What source of energy do you use?
     What is your atmosphere made of?
     Can you send us a ship?
