8d269460caf9feec46457e2a934e5e023475ff25
Sara Jahanian <Sara@Saras-MBP-4.fios-router.home>
2017-12-10 21:38:14 -0500

    Formatting attempt #389

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,22 +1,16 @@
-"Hello, aliens! If you understand this, you 
-likely can contribute more to our civilization than 
-we can to yours. However, here are some digits of what we call 'pi': 
-  3.1415926535897932384626433832795028841971693993751058209749445
-  9230781640628620899862803482534211706798214808651328230664709
-  38446095505822317253594081284811174502841027019385211055596446
-
-We have plenty of questions for you from here on Earth: 
-
-What source of energy do you use?
-What is your atmosphere made of?
-Can you send us a ship?
-Which came first the chicken or the egg?
-
-Here is a link to arguably the best music video ever made: 
-  
-  https://www.youtube.com/watch?v=djV11Xbc914
-
-Please send us something back so we know you received this message!
-  
-Sincerely,
-Your friends from Earth"
+cat("Hello, aliens! If you understand this, you 
+    likely can contribute more to our civilization than 
+    we can to yours. However, here are some digits of what we call 'pi':",
+    3.14159265358979323846264338327950288419716939937510582097494459230781
+    6406286208998628034825342117067982148086513282306647093844609550582231
+    7253594081284811174502841027019385211055596446, 
+    "We have plenty of questions for you from here on Earth: 
+    What source of energy do you use?
+    What is your atmosphere made of?
+    Can you send us a ship?
+    Which came first the chicken or the egg?
+    Here is a link to arguably the best music video ever made: 
+    https://www.youtube.com/watch?v=djV11Xbc914
+    Please send us something back so we know you received this message!
+    Sincerely,
+    Your friends from Earth")
